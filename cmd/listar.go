32354ea51b3760a02fda53dd77bbdea6aeee4636
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2020-08-14 10:21:30 -0300

    refactor

--- cmd/listar.go
+++ cmd/listar.go
@@ -9,14 +9,9 @@ import (
 	"regexp"
 )
 
-// 	Black   = Color("\033[1;30m%s\033[0m")
-// 	Red     = Color("\033[1;31m%s\033[0m")
-// 	Green   = Color("\033[1;32m%s\033[0m")
-// 	Yellow  = Color("\033[1;33m%s\033[0m")
-// 	Purple  = Color("\033[1;34m%s\033[0m")
-// 	Magenta = Color("\033[1;35m%s\033[0m")
-// 	Teal    = Color("\033[1;36m%s\033[0m")
-// 	White   = Color("\033[1;37m%s\033[0m")
+const regex = `^http[s]?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\/([-a-zA-Z0-9@:%._\+~#=]{1,32})\/([a-zA-Z-]*)`
+
+var projectRegex = regexp.MustCompile(regex)
 
 type MergeRequest struct {
 	State          string `json:"state"`
@@ -26,20 +21,9 @@ type MergeRequest struct {
 }
 
 func ListarMergeRequest(urlApi, token, state string, minComents int) {
-	// endpoint := fmt.Sprintf("%s%s", urlApi, "/projects/mov%2fmov-react-native/merge_requests?state=opened")
-	stateQuery := ""
-	if len(state) > 0 {
-		stateQuery = fmt.Sprintf("?state=%s", state)
-	}
-	endpoint := fmt.Sprintf("%s/%s%s", urlApi, "merge_requests", stateQuery)
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-	req, _ := http.NewRequest("GET", endpoint, nil)
-	req.Header.Set("PRIVATE-TOKEN", token)
-	resp, err := client.Do(req)
+	var endpoint = obterEndpoint(state, urlApi)
 
+	resp, err := executarGet(endpoint, token)
 	if err != nil {
 		fmt.Printf("URL inválida: %v\n", err)
 		return
@@ -51,36 +35,64 @@ func ListarMergeRequest(urlApi, token, state string, minComents int) {
 		fmt.Printf("Falha ao ler resposta: %v\n", err)
 		return
 	}
-	if resp.StatusCode < http.StatusBadRequest {
-		var data []MergeRequest
-		err := json.Unmarshal(body, &data)
-		if err != nil {
-			fmt.Printf("Falha ao decodificar a resposta: %v\n", err)
-			return
-		}
-		r := regexp.MustCompile(`^https:\/\/.*.intranet.bb.com.br\/(.{3})\/([a-zA-Z-]*)`)
-		found := false
-		for _, mr := range data {
-			match := r.FindStringSubmatch(mr.WebUrl)
-			projeto := ""
-			if len(match) == 3 {
-				projeto = match[2]
-			}
-			if mr.UserNotesCount > minComents {
-				fmt.Print(fmt.Sprintf("\033[1;32m[%s]\033[0m ", projeto))
-				if state != "opened" {
-					fmt.Print(fmt.Sprintf("\033[1;33m%s\033[0m ", mr.State))
-				}
-				fmt.Print(fmt.Sprintf("MR: %s ", mr.Title))
-				fmt.Println(fmt.Sprintf("\033[1;36m[%s]\033[0m", mr.WebUrl))
-				found = true
-			}
-		}
-		if !found {
-			fmt.Println("Nenhum Merge Request encontrado.")
-		}
 
+	if resp.StatusCode < http.StatusBadRequest {
+		validarResposta(body, minComents)
 	} else {
 		fmt.Printf("Não foi possível obter a lista de MRs: %s\n", string(body))
 	}
+}
+
+func executarGet(endpoint, token string) (*http.Response, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	req, _ := http.NewRequest("GET", endpoint, nil)
+	req.Header.Set("PRIVATE-TOKEN", token)
+	return client.Do(req)
+}
+
+func obterEndpoint(state string, urlApi string) string {
+	stateQuery := ""
+	if len(state) > 0 {
+		stateQuery = fmt.Sprintf("?state=%s", state)
+	}
+	return fmt.Sprintf("%s/%s%s", urlApi, "merge_requests", stateQuery)
+}
+
+func validarResposta(body []byte, minComents int) {
+	var mergeRequests []MergeRequest
+	err := json.Unmarshal(body, &mergeRequests)
+	if err != nil {
+		fmt.Printf("Falha ao decodificar a resposta: %v\n", err)
+		return
+	}
+	found := false
+	for _, mr := range mergeRequests {
+		if mr.UserNotesCount > minComents {
+			exibirResultado(&mr)
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Nenhum Merge Request encontrado.")
+	}
+}
+
+func exibirResultado(mr *MergeRequest) {
+	fmt.Print(fmt.Sprintf("\033[1;32m[%s]\033[0m ", obterNomeProjeto(mr.WebUrl)))
+	if mr.State != "opened" {
+		fmt.Print(fmt.Sprintf("\033[1;33m%s\033[0m ", mr.State))
+	}
+	fmt.Print(fmt.Sprintf("MR: %s ", mr.Title))
+	fmt.Println(fmt.Sprintf("\033[1;36m[%s]\033[0m", mr.WebUrl))
+}
+
+func obterNomeProjeto(url string) string {
+	match := projectRegex.FindStringSubmatch(url)
+	if len(match) == 3 {
+		return match[2]
+	}
+	return ""
 }
